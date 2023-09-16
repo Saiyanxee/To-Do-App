96e1cfb1682bf4fa8d48f4f5bb142ac1e09667e9
Basit <basitnawaz782@gmail.com>
2023-09-16 07:42:36 +0500

    Edited add and edit feature

--- gui.py
+++ gui.py
@@ -3,13 +3,14 @@ import PySimpleGUI as sg
 
 label = sg.Text("Type in a text")
 input_box = sg.InputText(tooltip='enter a text',key='todo')
+list_box=sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True,
+                    size=[45, 10])
+edit_but=sg.Button("Edit")
 add_button = sg.Button("Add")
 del_but = sg.Button("delete")
-list_box=sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True,
-                    size=[10, 10])
-edit_but=sg.Button("Edit")
+
 window = sg.Window('APP BY SAIYAN',
-                   layout=[[label, input_box, add_button],
+                   layout=[[label], [input_box, add_button],
                            [list_box,edit_but],[del_but]],
                    font=("Helvetica", 20))
 
@@ -23,6 +24,17 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.writing_todos(todos)
+            window['todos'].update(values=todos)
+        case 'Edit':
+            todo_to_edit=values['todos'][0]
+            edited=values['todo']
+            todos=functions.get_todos()
+            index=todos.index(todo_to_edit)
+            todos[index]=edited
+            functions.writing_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WINDOW_CLOSED:
             break
 
