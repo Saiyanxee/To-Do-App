2efca93d7eec6af6d0aa73578db73fb9d77e7851
Basit <basitnawaz782@gmail.com>
2023-09-19 12:16:01 +0500

    Complete and Exit button

--- gui.py
+++ gui.py
@@ -2,22 +2,24 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a text")
-input_box = sg.InputText(tooltip='enter a text',key='todo')
+input_box = sg.InputText(tooltip='enter a text', key='todo')
 list_box=sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True,
                     size=[45, 10])
 edit_but=sg.Button("Edit")
 add_button = sg.Button("Add")
-del_but = sg.Button("delete")
+Comp_but = sg.Button("Complete")
+Ex_but = sg.Button("Exit")
 
 window = sg.Window('APP BY SAIYAN',
                    layout=[[label], [input_box, add_button],
-                           [list_box,edit_but],[del_but]],
+                           [list_box, edit_but, Comp_but],
+                           [Ex_but]],
                    font=("Helvetica", 20))
 
 while True:
     events, values = window.read()
-    print(1,events)
-    print(2,values)
+    print(1, events)
+    print(2, values)
     match events:
         case 'Add':
             todos = functions.get_todos()
@@ -33,6 +35,15 @@ while True:
             todos[index]=edited
             functions.writing_todos(todos)
             window['todos'].update(values=todos)
+        case 'Complete':
+            todo_to_comp = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_comp)
+            functions.writing_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case 'Exit':
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WINDOW_CLOSED:
